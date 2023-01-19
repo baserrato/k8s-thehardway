e1ce1bfe5036249a33615c754a0dc958c35bac44
baserrato <benjamins@liatrio.com>
2023-01-19 11:06:28 -0800

    ci: add new workflow for validation

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "client-secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "tenant-id" {
+  type      = string
+  sensitive = true
+}
+
+variable "client-id" {
+  type      = string
+  sensitive = true
+}
+
+variable "subscription-id" {
+  type      = string
+  sensitive = true
+}
