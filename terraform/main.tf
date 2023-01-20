e85fbfd8f12906879b1be2bcf9c0f53f9eb5d480
Benjamin Serrato <54423854+baserrato@users.noreply.github.com>
2023-01-20 08:30:57 -0800

    build: worker nodes setup (#2)
    * connected load balancer pool to network interface
    
    * build: worker nodes setup

--- terraform/main.tf
+++ terraform/main.tf
@@ -119,11 +119,12 @@ resource "azurerm_lb" "kubernetes-lb" {
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "example" {
+resource "azurerm_lb_backend_address_pool" "kubernetes-lb-pool" {
   loadbalancer_id = azurerm_lb.kubernetes-lb.id
   name            = "kubernetes-lb-pool"
 }
 
+# Setting up controller nodes
 resource "azurerm_availability_set" "controller-as" {
   name                = "controller-as"
   location            = azurerm_resource_group.kubernetes.location
@@ -167,6 +168,13 @@ resource "azurerm_network_interface" "controller-nic" {
   }
 }
 
+resource "azurerm_network_interface_backend_address_pool_association" "kubernetes-lb-nic-association" {
+  count                   = 3
+  network_interface_id    = azurerm_network_interface.controller-nic[count.index].id
+  ip_configuration_name   = "internal"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.kubernetes-lb-pool.id
+}
+
 resource "azurerm_linux_virtual_machine" "controller-vm" {
   count                 = 3
   name                  = "controller-${count.index}"
@@ -197,6 +205,85 @@ resource "azurerm_linux_virtual_machine" "controller-vm" {
 
 resource "tls_private_key" "kubernetes-controller-key" {
   count     = 3
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+# Setting up Worker nodes
+resource "azurerm_availability_set" "worker-as" {
+  name                = "worker-as"
+  location            = azurerm_resource_group.kubernetes.location
+  resource_group_name = azurerm_resource_group.kubernetes.name
+
+  tags = {
+    Client      = azurerm_resource_group.kubernetes.tags.Client
+    Project     = azurerm_resource_group.kubernetes.tags.Project
+    Owner       = azurerm_resource_group.kubernetes.tags.Owner
+    Application = azurerm_resource_group.kubernetes.tags.Application
+  }
+}
+
+
+resource "azurerm_public_ip" "kubernetes-pip-workers" {
+  count               = 2
+  name                = "worker-${count.index}-pip"
+  location            = azurerm_resource_group.kubernetes.location
+  resource_group_name = azurerm_resource_group.kubernetes.name
+  allocation_method   = "Static"
+
+  tags = {
+    Client      = azurerm_resource_group.kubernetes.tags.Client
+    Project     = azurerm_resource_group.kubernetes.tags.Project
+    Owner       = azurerm_resource_group.kubernetes.tags.Owner
+    Application = azurerm_resource_group.kubernetes.tags.Application
+  }
+}
+
+resource "azurerm_network_interface" "worker-nic" {
+  count               = 2
+  name                = "worker-${count.index}-nic"
+  location            = azurerm_resource_group.kubernetes.location
+  resource_group_name = azurerm_resource_group.kubernetes.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.kubernetes-subnet.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.240.0.1${count.index}"
+    public_ip_address_id          = azurerm_public_ip.kubernetes-pip-workers[count.index].id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "worker-vm" {
+  count                 = 2
+  name                  = "worker-${count.index}"
+  location              = azurerm_resource_group.kubernetes.location
+  resource_group_name   = azurerm_resource_group.kubernetes.name
+  network_interface_ids = [azurerm_network_interface.worker-nic[count.index].id]
+  admin_username        = "kuberoot"
+  availability_set_id   = azurerm_availability_set.worker-as.id
+
+  size = "Standard_F2"
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "18.04.202301100"
+  }
+
+  admin_ssh_key {
+    username   = "kuberoot"
+    public_key = tls_private_key.kubernetes-worker-key[count.index].public_key_openssh
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+resource "tls_private_key" "kubernetes-worker-key" {
+  count     = 2
   algorithm = "RSA"
   rsa_bits  = 4096
 }
